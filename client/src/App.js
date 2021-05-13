629cad9f6ca530d9b25d6db300b21b5887e78ced
David Ukelere <david.ukelere291@gmail.com>
2021-05-13 20:48:01 +0100

    Setup react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home />
+          <Route component={Home} exact path="/" />
+          <Route component={Products} path="/product/:id" />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
